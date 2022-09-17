4e56f5cbe78fd31778a5f41c80cd2701b5136a78
Victor Prudnikov <victor.prudnikov@gmail.com>
2022-09-17 12:45:09 +0200

    Initial commit for homework 1.2 Main tread and Background

---
+++ README.md
@@ -0,0 +1,3 @@
+## Домашнее задание к занятию «1.2. Сетевые запросы: Main Thread & Background»
+### Задача Likes
+### Задача Swipe to Refresh*
---
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -0,0 +1,116 @@
+package ru.netology.nmedia.viewmodel
+
+import  android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.model.FeedModel
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryImpl
+import ru.netology.nmedia.util.SingleLiveEvent
+import java.io.IOException
+import kotlin.concurrent.thread
+
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    likedByMe = false,
+    likes = 0,
+    published = ""
+)
+
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    // упрощённый вариант
+    private val repository: PostRepository = PostRepositoryImpl()
+    private val _data = MutableLiveData(FeedModel())
+    val data: LiveData<FeedModel>
+        get() = _data
+    val edited = MutableLiveData(empty)
+    private val _postCreated = SingleLiveEvent<Unit>()
+    val postCreated: LiveData<Unit>
+        get() = _postCreated
+
+    init {
+        loadPosts()
+    }
+
+    fun loadPosts() {
+        thread {
+            // Начинаем загрузку
+            _data.postValue(FeedModel(loading = true))
+            try {
+                // Данные успешно получены
+                val posts = repository.getAll()
+                FeedModel(posts = posts, empty = posts.isEmpty())
+            } catch (e: IOException) {
+                // Получена ошибка
+                FeedModel(error = true)
+            }.also(_data::postValue)
+        }
+    }
+
+    fun save() {
+        edited.value?.let {
+            thread {
+                repository.save(it)
+                _postCreated.postValue(Unit)
+            }
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
+        edited.value = edited.value?.copy(content = text)
+    }
+
+    fun likeById(id: Long) {
+        val postToLike = _data.value?.posts.orEmpty().filter { it.id == id }
+        thread {
+            var likedPost: Post = if (!postToLike[0].likedByMe) {
+                repository.likeById(id)
+            } else {
+                repository.delLikeById(id)
+            }
+            val postsNewLiked = _data.value?.posts.orEmpty().map { it ->
+                if (it.id === likedPost.id) {
+                    Post(
+                        id = likedPost.id,
+                        author = likedPost.author,
+                        content = likedPost.content,
+                        published = likedPost.published,
+                        likedByMe = likedPost.likedByMe,
+                        likes = likedPost.likes
+                    )
+                } else it
+            }
+            _data.postValue(FeedModel(posts = postsNewLiked))
+        }
+    }
+
+    fun removeById(id: Long) {
+        thread {
+            // Оптимистичная модель
+            val old = _data.value?.posts.orEmpty()
+            _data.postValue(
+                _data.value?.copy(posts = _data.value?.posts.orEmpty()
+                    .filter { it.id != id }
+                )
+            )
+            try {
+                repository.removeById(id)
+            } catch (e: IOException) {
+                _data.postValue(_data.value?.copy(posts = old))
+            }
+        }
+    }
+}
